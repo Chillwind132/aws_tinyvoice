eb9b845a02a2e5c37691b6f56b7992b0733e270f
Chillwind132 <99220831+Chillwind132@users.noreply.github.com>
2022-06-19 21:26:59 -0400

    Update

--- tinyvoice.py
+++ tinyvoice.py
@@ -16,6 +16,7 @@ import sounddevice as sd
 import soundfile as sf
 import os
 import queue
+import threading
 
 assert np
 
@@ -53,14 +54,48 @@ async def basic_transcribe():
         async with aiofile.AIOFile('voice.wav', 'rb') as afp:
             reader = aiofile.Reader(afp, chunk_size=1024 * 16)
             async for chunk in reader:
+                time.sleep(0.5)
                 await stream.input_stream.send_audio_event(audio_chunk=chunk)
+        
         await stream.input_stream.end_stream()
 
     # Instantiate our handler and start processing events
     handler = MyEventHandler(stream.output_stream)
     await asyncio.gather(write_chunks(), handler.handle_events())
 
+
+class myThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+
+   def run(self):
+       print("Starting thread 1")
+       while True:
+            time.sleep(0.5)
+            print("Exiting " + self.name)
+            if record_flag is True:
+
+                time.sleep(1.5)
+
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+                loop.run_until_complete(basic_transcribe())
+                loop.close()
+            
+
+            if stop_threads:
+               return
+
+
 if __name__ == "__main__":
+
+    global stop_threads, record_flag
+    record_flag = False
+    stop_threads = True
+
     with open('rootkey.csv', 'r') as f:
         content = f.readlines()
 
@@ -71,6 +106,10 @@ if __name__ == "__main__":
 
     AWS_ACCESS_KEY_ID = keys['AWSAccessKeyId']
     AWS_SECRET_KEY = keys['AWSSecretKey']
+
+
+    thread1 = myThread(1, "Thread-1", 1)
+    thread1.start()
 
     q = queue.Queue()
     
@@ -84,12 +123,13 @@ if __name__ == "__main__":
             print('press Ctrl+C to stop the recording')
             print('#' * 80)
             while True:
+                record_flag = True
                 file.write(q.get())
+    
+    
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(basic_transcribe())
-    loop.close()
-
+    
+    
     
     print('Done')
 
