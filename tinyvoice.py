3d73a54e35728d96c72603e5b978f03dd6c23d61
Chillwind132 <99220831+Chillwind132@users.noreply.github.com>
2022-06-20 22:01:48 -0400

    Code cleanup

--- tinyvoice.py
+++ tinyvoice.py
@@ -1,30 +1,21 @@
-from amazon_transcribe.model import TranscriptEvent
-from amazon_transcribe.handlers import TranscriptResultStreamHandler
-from amazon_transcribe.client import TranscribeStreamingClient
 import aiofile
 import asyncio
 import time
 import boto3
-import sounddevice as sd
-import soundfile as sf
-import numpy
 import sys
-import queue
 import tempfile
 import numpy as np
-import sounddevice as sd
-import soundfile as sf
 import os
 import queue
 import threading
-
-assert np
+import sounddevice as sd
+import soundfile as sf
+from amazon_transcribe.model import TranscriptEvent
+from amazon_transcribe.handlers import TranscriptResultStreamHandler
+from amazon_transcribe.client import TranscribeStreamingClient
 
 
 def callback(indata, frames, time, status):
-    """
-    This is called from a separate thread for each audio block
-    """
     if status:
         print(status, file=sys.stderr)
     q.put(indata.copy())
@@ -32,8 +23,6 @@ def callback(indata, frames, time, status):
 
 class MyEventHandler(TranscriptResultStreamHandler):
     async def handle_transcript_event(self, transcript_event: TranscriptEvent):
-        # This handler can be implemented to handle transcriptions as needed.
-        # Here's an example to get started.
         results = transcript_event.transcript.results
         for result in results:
             for alt in result.alternatives:
@@ -42,7 +31,6 @@ class MyEventHandler(TranscriptResultStreamHandler):
 
 async def basic_transcribe():
     client = TranscribeStreamingClient(region="us-east-2")
-
     # Start transcription to generate our async stream
     stream = await client.start_stream_transcription(
         language_code="en-US",
@@ -58,7 +46,6 @@ async def basic_transcribe():
                 await stream.input_stream.send_audio_event(audio_chunk=chunk)
         
         await stream.input_stream.end_stream()
-
     # Instantiate our handler and start processing events
     handler = MyEventHandler(stream.output_stream)
     await asyncio.gather(write_chunks(), handler.handle_events())
@@ -75,7 +62,6 @@ class myThread (threading.Thread):
        print("Starting thread 1")
        while True:
             time.sleep(0.5)
-            print("Exiting " + self.name)
             if record_flag is True:
 
                 time.sleep(1.5)
@@ -84,54 +70,61 @@ class myThread (threading.Thread):
                 asyncio.set_event_loop(loop)
                 loop.run_until_complete(basic_transcribe())
                 loop.close()
-            
-
             if stop_threads:
                return
 
 
+class main():
+    def __init__(self):
+
+        self.AWS_ACCESS_KEY_ID = ""
+        self.AWS_SECRET_KEY = ""
+
+        self.main()
+
+    def auth(self):
+        with open('rootkey.csv', 'r') as f:
+            content = f.readlines()
+
+        keys = {}
+        for line in content:
+            pair = line.strip().split('=')
+            keys.update({pair[0]: pair[1]})
+
+        self.AWS_ACCESS_KEY_ID = keys['AWSAccessKeyId']
+        self.AWS_SECRET_KEY = keys['AWSSecretKey']
+
+    def main(self):
+
+        self.auth()
+
+        global stop_threads, record_flag
+        record_flag = False
+        stop_threads = True
+
+        thread1 = myThread(1, "Thread-1", 1)
+        thread1.start()
+
+        filename = 'voice.wav'
+        if os.path.isfile(filename):
+            os.remove(filename)
+
+        with sf.SoundFile(filename, mode='x', samplerate=16000, channels=1) as file:
+            with sd.InputStream(samplerate=16000, channels=1, callback=callback):
+                print('#' * 80)
+                print('press Ctrl+C to stop the recording')
+                print('#' * 80)
+                while True:
+                    record_flag = True
+                    file.write(q.get())
+  
 if __name__ == "__main__":
-
-    global stop_threads, record_flag
-    record_flag = False
-    stop_threads = True
-
-    with open('rootkey.csv', 'r') as f:
-        content = f.readlines()
-
-    keys = {}
-    for line in content:
-        pair = line.strip().split('=')
-        keys.update({pair[0]: pair[1]})
-
-    AWS_ACCESS_KEY_ID = keys['AWSAccessKeyId']
-    AWS_SECRET_KEY = keys['AWSSecretKey']
-
-
-    thread1 = myThread(1, "Thread-1", 1)
-    thread1.start()
-
+    
     q = queue.Queue()
-    
-    filename = 'voice.wav'
-    if os.path.isfile(filename):
-        os.remove(filename)
-    
-    with sf.SoundFile(filename, mode='x', samplerate=16000, channels=1) as file:
-        with sd.InputStream(samplerate=16000, channels=1, callback=callback):
-            print('#' * 80)
-            print('press Ctrl+C to stop the recording')
-            print('#' * 80)
-            while True:
-                record_flag = True
-                file.write(q.get())
-    
-    
+    main()
 
-    
-    
-    
-    print('Done')
+
+
 
 
 
